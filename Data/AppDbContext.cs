817c68fab2b970e2942af4d76e15c6edf4650fc2
RonmelT123 <a2019210835@estudiantes.upsa.edu.bo>
2021-10-11 10:50:33 -0400

    crear AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using canciones_web_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace canciones_web_api.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Cancion> Cancion { get; set; }
+    }
+}
